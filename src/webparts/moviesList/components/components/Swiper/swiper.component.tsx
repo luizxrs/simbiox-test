9a41196250245afd0070cfa20e4cd5b09c61019d
luizxrs <lgustavoitp@gmail.com>
2024-04-24 22:29:54 -0300

    feat: added type to the swiper comp

--- src/webparts/moviesList/components/components/Swiper/swiper.component.tsx
+++ src/webparts/moviesList/components/components/Swiper/swiper.component.tsx
@@ -2,10 +2,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import React from "react";
 
 import "swiper/css";
+import { getPopular } from "../../../../../types/fetch/getPopular";
+import { getPoster } from "../../../../../lib/get/getPoster";
 
-interface ISwiperComponent {}
+interface ISwiperComponent {
+  data: getPopular;
+}
 
-export const SwiperComponent = ({}: ISwiperComponent[]) => {
+export const SwiperComponent = ({ data }: ISwiperComponent) => {
   return (
     <Swiper
       spaceBetween={50}
@@ -13,8 +17,11 @@ export const SwiperComponent = ({}: ISwiperComponent[]) => {
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      ...
+      {data.results.map((movie, i) => {
+        <SwiperSlide key={i}>
+          <img src={getPoster(movie.poster_path)} alt="" />
+        </SwiperSlide>;
+      })}
     </Swiper>
   );
 };
